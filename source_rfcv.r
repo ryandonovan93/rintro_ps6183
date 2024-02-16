b94fd34ee0530a4802f678dbce1236c4edfa34e1
Ryan Donovan <ryan.br.donovam@gmail.com>
2024-02-16 17:40:46 +0000

    Added content to chapter 4

---
+++ source_rfcv.r
@@ -0,0 +1,5 @@
+structure(list(name = "aggregate", objs = list("package:stats" = function (x, 
+    ...) 
+UseMethod("aggregate"), function (x, ...) 
+UseMethod("aggregate")), where = c("package:stats", "namespace:stats"
+), visible = c(TRUE, FALSE), dups = c(FALSE, TRUE)), class = "getAnywhere")
