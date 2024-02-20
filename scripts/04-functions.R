5d0d16581f1fbf619875c7493f82ab13b4fa15cb
Ryan Donovan <ryan.br.donovam@gmail.com>
2024-02-20 15:05:56 +0000

    Proofread Chapter 4 & Added Glossary

---
+++ scripts/04-functions.R
@@ -0,0 +1,77 @@
+
+# Creating Functions in R -------------------------------------------------
+
+
+# Creating a Simple Function (1-Argument) ---------------------------------
+
+#cups_to_grams
+
+#test it
+
+#define cups outside of the function
+
+
+
+# Creating a Multi-Argument Function ---------------------------------
+
+
+#calculate z score
+#arguments: x, mean_val, sd_val
+#formula z = (x - mean) / sd
+
+#iq example: c(100, 130, 85)
+
+#create the iq_df dataframe
+
+#create iq_z_scores vector
+
+#dataframe$newColumnName <- new_vector
+
+
+# Functions inside Functions ----------------------------------------------
+
+#total_depression_beck vector: c(32, 36, 34, 46,
+#30, 53, 40, 15)
+
+#calculate:z-score -> order of function
+
+
+#calculate:z-score -> fix it
+
+
+#calculate_z_score(x = iq_df$total_depression_beck)
+
+
+
+# Returning Multiple Objects from a Function ------------------------------
+
+#lists
+
+#tidy up list output: new_name = value
+
+#save to scores
+
+#extract each element from scores
+
+
+
+# Important Features about Functions --------------------------------------
+
+
+#local vs global
+#num`1
+
+
+#Default arguments
+#greet()
+
+
+
+
+
+# Exercises ---------------------------------------------------------------
+
+
+
+
+
